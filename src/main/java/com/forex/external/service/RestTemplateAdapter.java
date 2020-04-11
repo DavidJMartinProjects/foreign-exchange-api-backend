b7e07496929296ea410430888a196e4fed2a3029
unknown <davidjmartin@hotmail.com>
2020-04-11 20:39:10 +0100

    Refactorings

--- src/main/java/com/forex/external/service/RestTemplateAdapter.java
+++ src/main/java/com/forex/external/service/RestTemplateAdapter.java
@@ -21,11 +21,11 @@ public class RestTemplateAdapter {
 	private SupportedCurrencies supportedCurrencies;
 
 	public SupportedCurrencies performGetRequest(final String url) {
+		String responseBody = new RestTemplate().exchange(url, HttpMethod.GET, requestUtil.buildHeaders(), String.class).getBody();
 		try {
-			String responseBody = new RestTemplate().exchange(url, HttpMethod.GET, requestUtil.buildHeaders(), String.class).getBody();
 			supportedCurrencies = new ObjectMapper().readValue(responseBody, SupportedCurrencies.class);
 		} catch (IOException exception) {
-			log.info("error processing request from external service.", exception.getMessage(), exception.getCause());
+			log.info("error processing response from external service.", exception.getMessage(), exception.getCause());
 		}
 		return supportedCurrencies;
 	}
